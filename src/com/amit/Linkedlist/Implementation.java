3e5ca869a31cf28674f382dbbba566c26e4b8851
amitkumar <amitkumar@localhost>
2024-01-01 02:55:18 +0530

    Addfrist, AddLast and printList

---
+++ src/com/amit/Linkedlist/Implementation.java
@@ -0,0 +1,70 @@
+package com.amit.Linkedlist;
+
+public class Implementation {
+	public Node head=null;
+	private int size;
+	
+
+	public class Node{
+		String data;
+		Node next;
+		Node(String data){
+			this.data=data;
+			this.next=null;
+			
+		}
+	}	
+	
+	// Add Node in first place
+	public void addfirst(String data) {
+		Node newNode=new Node(data);
+		if(head==null) {
+			head=newNode;
+			return;
+		}
+		newNode.next=head;
+		head=newNode;
+	}
+	
+	// Add Node in Last place 
+	public void addLast(String data) {
+		Node newNode=new Node(data);
+		if(head==null) {
+			head=newNode;
+			return;
+		}
+		Node currNode=head;
+		while(currNode.next!=null) {
+			currNode=currNode.next;
+			
+		}
+	      currNode.next=newNode;
+	}
+	// Print List 
+	public void printList() {
+		Node currNode=head;
+		while(currNode!=null) {
+			System.out.print(currNode.data+"-->");
+			currNode=currNode.next;
+		}
+		System.out.println("null");
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Implementation imp=new Implementation();
+		imp.addfirst("a");
+		imp.addfirst("b");
+		imp.addfirst("c");
+		imp.addfirst("d");
+		imp.printList();
+		imp.addLast("e");
+		imp.addLast("f");
+		imp.printList();
+		
+
+	}
+
+}
